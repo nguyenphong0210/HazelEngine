6e95261fc6805fae8a57c6b20f3c8bb4393b057a
Nguyen Phong <nguyenphong0210pt@gmail.com>
2023-12-21 21:20:56 +0700

    add GLFW

--- Hazel/src/Hazel/Events/Event.h
+++ Hazel/src/Hazel/Events/Event.h
@@ -1,9 +1,9 @@
 #pragma once
+#include "hzpch.h"
 
 #include "Hazel/Core.h"
 
-#include <string>
-#include <functional>
+
 
 namespace Hazel {
 	// Events in Hazel are currently blocking, meaning when an event occurs it
